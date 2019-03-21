8e7c890088a4d447e8cfe5f7e3236f46d19b4ea9
GA Denver <gadenver@Generals-MacBook-Pro.local>
2019-03-21 13:27:16 -0600

    Commit 6 Yell at the Ninja Turtles answered

---
+++ 190320-stuff/js/app.js
@@ -0,0 +1,72 @@
+
+
+// 	1.
+
+//	for (let i = 1; i <= 20; i++) {
+//		console.log(i);
+//	}
+
+// 	2.
+
+//	for (let i = 1; i <= 200; i++) {
+//		if (i % 2 === 0) {
+//			console.log(i);
+//		};
+//	};
+
+/*	3.
+	let response = ["... human, why are you taking pictures of me?",
+			   	  "... Why does the red dot always get away?",
+				  "... the catniip made me do it..."]
+	for (let i = 0; i <= 20; i++) {
+		console.log("Love me, Pet me! HSSSSSS!"	);
+		if (i % 2 == 0) {console.log(response[Math.floor(Math.random() * 3)])};	
+	};
+
+
+
+for(let i = 1; i <= 100; i++) {
+	if (i % 3 === 0 && i % 5 === 0) {
+		console.log('FizzBuzz')
+	} else {
+	if ( i % 3 === 0) {
+		console.log('Fizz')
+	} else { if (i % 5 === 0) {
+		console.log('Buzz')
+	} else {
+	console.log(i)	
+	};
+	};
+	};
+}; 
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+shahzad[0] = "Gameboy"
+daniel[1] = daniel[1]++;
+ryan[2] = "Gotham City"
+reuben.pop()
+reuben.push('Chicago')
+jim.pop(); jim.push("Denver", "Chicago", "New York")
+jim.splice(2,1)
+console.log(jim)
+
+*/
+
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"]
+
+for(i = 0; i < ninjaTurtles.length; i++) {
+	ninjaTurtles[i] = ninjaTurtles[i].toUpperCase()		
+};
+
+console.log(ninjaTurtles);
+
+
+
+
+
+
